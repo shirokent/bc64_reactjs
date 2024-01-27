389002bead9abbb4da443ee0f92a571cb64c5ba2
DatNguyen <datnguyen.060997@gmail.com>
2024-01-27 09:58:48 +0700

    buoi1 reactjs

---
+++ src/component/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// function component
+export const Card = () => {
+  // Nội dung của thẻ Card component (Chỉ được bao phủ bởi 1 thẻ duy nhất)
+
+  return (
+    <div>
+      Hello lớp bc64
+      <br />
+      <div>Happy hacking</div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Card } from "./component/Card";
+import { AlertComponent } from "./component/AlertComponent";
+import HomeLayout from "./component/HomeLayout/HomeLayout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // jsx: => html được viết trong file js (nền js) được gọi là jsx
-root.render(<div>abc</div>);
+root.render(
+  <div>
+    {/* <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card /> */}
+
+    {/* <AlertComponent /> */}
+    <HomeLayout />
+  </div>
+);
+
+// Có 2 loại component: function component, class component
